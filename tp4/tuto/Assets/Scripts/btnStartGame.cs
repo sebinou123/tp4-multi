3acbba49135cbaf422eb98e8f9b85e7883204ccb
sebinou123 <sebman2009@hotmail.com>
2015-12-04 09:08:10 -0500

    edit btnstart

--- tp4/tuto/Assets/Scripts/btnStartGame.cs
+++ tp4/tuto/Assets/Scripts/btnStartGame.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class btnStartGame : MonoBehaviour {
 
-	public static GameManager game;	
+	public static GameManager game;
+	public static string name;
 
 	public void startGame(){
-		
-		Destroy (GameObject.Find ("CanvasCreateHero(Clone)"));
-		Destroy (GameObject.Find ("scriptCreateHero"));
-		game = GameObject.Find ("GameManager(Clone)").GetComponent<GameManager>();
-		game.InitGame();
+		InputField i = GameObject.Find ("CanvasCreateHero(Clone)").GetComponentInChildren<InputField>();
+	
+		if (i.text.ToString ().Length != 0) {
+			name = i.text.ToString ();
+			Destroy (GameObject.Find ("CanvasCreateHero(Clone)"));
+			Destroy (GameObject.Find ("scriptCreateHero"));
+			game = GameObject.Find ("GameManager(Clone)").GetComponent<GameManager> ();
+			game.InitGame ();
+		}
 	}
 }
